c532d681d503e096cfd19e7ccd1521d1f0bbf3cc
dajuno <dajuno@riseup.net>
2015-07-27 20:22:42 -0300

    fiddle with ODE... check dim and split x/y/z comp

--- spin.py
+++ spin.py
@@ -18,17 +18,15 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.integrate import ode
+from scipy.integrate import ode, odeint
 # import warning
 
 
 class spin:
 
     """ spin main class. """
-    # gyromagnetic ratio of protons (¹H):
-    G = 42.6  # MHz/Tesla
 
-    def __init__(self, M0=1, B0=3, w0=0, dw=0, T1=200, T2=600,
+    def __init__(self, M0=1, B0=3, w0=0, dw=0, tend=1, T1=0.200, T2=0.600,
                  Minit=[0.6, 0, 0.8]):
         """ constructor
         M0      equilibrium magnetization
@@ -39,59 +37,89 @@ class spin:
         T1      relaxation time of substance
         T2
         """
+    # gyromagnetic ratio of protons (¹H):
+        self.g = 42.6e6  # Hz/Tesla
         self.M0 = M0
         self.B0 = B0
         if w0 == 0:
-            self.w0 = -G*B0
+            self.w0 = -self.g*B0
         else:
             self.w0 = w0
         self.dw = dw
+        self.tend = tend
         self.T1 = T1
         self.T2 = T2
         self.Minit = Minit
 
     def pulse_seq(self, t):
         """ define RF pulse sequences """
-        return 0
+        return np.array([0, 0, 0])
 
-    def solve(self, backend='dopri5', nsteps=1, atol=1e-3):
+    def solve(self, backend='vode', nsteps=100, atol=1e-3):
         """ solve with scipy.integrate.ode
 
-        backend     dopri5, vode, dop853
-        nsteps      should be 1 for adaptive step size
+        backend     vode, dopri5, dop853
+        nsteps
         atol        required accuracy
         """
 
-        def rhs(t, y, *arg):
+        def rhs(t, y, self):
             """ assemble rhs of Bloch equation
             arg: self, B
 
             B       effective magnetic oscil. field
             R       relaxation terms
             """
-            self = arg[0]
-            T2 = self.T2, T1 = self.T1, M0 = self.M0
-            B = np.array([
-                0,
-                0,
-                self.B0 + self.pulse_seq(t)
-                ])
+            # self = arg[0]
+            T2 = self.T2
+            T1 = self.T1
+            M0 = self.M0
+            B0 = self.B0
+
+            omega = self.w0+self.dw
+            Brot = np.array([0, 0, omega/self.g])
+            B = np.array([0, 0, B0]) + self.pulse_seq(t) - Brot
             R = np.array([
                 y[0]/T2,
                 y[1]/T2,
                 (y[2]-M0)/T1
                 ])
+            print(self.g*np.cross(y, B) - R)
 
-            return G*np.cross(y, B) - R
+            return self.g*np.cross(y, B) - R
 
-        solver = ode(rhs).set_integrator(backend, nsteps=nsteps, atol=atol)
-        solver.set_initial_value(self.M0, 0)
-        solver.set_f_params((self))
-# suppress Fortran-printed warning
-        # solver._ingerator.iwork[2] = -1
-        # warnings.filterwarnings("ignore", category=UserWarning)
+        # solver = ode(rhs).set_integrator(backend, nsteps=nsteps, atol=atol)
 
-    def relaxation(self, ti=[0, 1000], dt=1):
+        ''' VAR 1 ##   automatic step size control '''
+        if backend == 'vode':
+            sol = []
+            t = []
+            solver = ode(rhs).set_integrator('vode', atol=atol)
+            solver.set_initial_value(self.Minit, 0)
+            solver.set_f_params(self)
+            while solver.successful() and solver.t < self.tend:
+                solver.integrate(self.tend, step=True)
+                t.append(solver.t)
+                sol.append(solver.y)
+                print("%g/%g" % (solver.t, self.tend))
+        elif backend == 'dopri5':
+            ''' VAR 2 ## automatic step size with time grid '''
+            sol = []
+            t = []
+            # t = np.linspace(0, self.tend, nsteps)
+            dt = self.tend/nsteps
+            solver = ode(rhs).set_integrator('dopri5', atol=atol)
+            solver.set_initial_value(self.Minit, 0)
+            solver.set_f_params(self)
+            while solver.successful() and solver.t < self.tend:
+                solver.integrate(solver.t+dt)
+                t.append(solver.t)
+                sol.append(solver.y)
+                print("%g/%g" % (solver.t, self.tend))
+
+        return t, sol
+
+    def relaxation(self, dt=1):
         ''' calculate and plot T1/T2 relaxation during free precession, within a
         rotating frame of reference, with w == w_0
         t   [t0, tend] in ms
@@ -101,7 +129,7 @@ class spin:
         # import time
 
 # T1, T2 relaxation
-        t = np.linspace(ti[0], ti[1], (ti[1]-ti[0])/dt, endpoint=True)
+        t = np.linspace(0, self.tend, self.tend/dt, endpoint=True)
         self.MR = np.array(np.zeros((len(t), 3)))
         # E1 = np.exp(-t/self.T1)
         # E2 = np.exp(-t/self.T2)
@@ -137,8 +165,46 @@ class spin:
         ax2.plot(t, self.MR[:, 2])
         ax2.set_title('T2 relaxation')
 
+
+def plot3Dtime(t, M, skip=10):
+    import time
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.axis([-1, 1, -1, 1])
+    ax.plot([0, 0], [0, 0], [-1,  1], '-.k')
+    ax.plot([-1, 1], [0, 0], [0, 0], '-.k')
+    ax.plot([0, 0], [-1, 1], [0, 0], '-.k')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    for i in range(len(t[::skip])):
+        ax.plot([0, M[i, 0]], [0, M[i, 1]], [0, M[i, 2]],
+                '-<r')
+        plt.draw()
+        time.sleep(0.1)
+
+
+def plot_relax(t, M):
+    plt.ion()
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.plot(t, M[:, 0], label='$M_x$')
+    ax1.plot(t, M[:, 1], label='$M_y$')
+    ax1.legend()
+    ax1.set_xlabel('time in ms')
+    ax1.set_ylabel('$M(t)$')
+    ax1.set_title('T1 relaxation')
+    ax2.plot(t, M[:, 2])
+    ax2.set_title('T2 relaxation')
+
 if __name__ == '__main__':
-    print("spinning..")
+    s = spin(dw=100)
+    t, y = s.solve(backend='dopri5', atol=1e-3)
+
+    # plot_relax(t, y)
+    # plot3Dtime(t, y)
+
 
 
 # ######### OLD :::: DELETE BELOW ############
