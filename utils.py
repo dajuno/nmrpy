34e4b2305a1a2733dedfdacce4f37e2ef7436f26
dajuno <dajuno@riseup.net>
2015-08-01 13:26:56 -0300

    progressbar nicer

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ import sys
 def progressbar(t, tend):
     # print(int(t/tend*10))
     amtDone = t/tend
-    sys.stdout.write("\rsolve: [{0:50s}] {1}/{2}".format('#' * int(amtDone *
+    sys.stdout.write("\rsolve: [{0:50s}] {1:.3f}/{2:.1f}".format('#' * int(amtDone *
                      50), t, tend))
     sys.stdout.flush()
